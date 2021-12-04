d36dff6604287fab0048d30d1375df50b7dd78a4
Elvin Hernández <94201127+ElvinAlejandro@users.noreply.github.com>
2021-12-04 03:13:23 -0600

    Cambios

--- Examen_1/Examen_1/Program.cs
+++ Examen_1/Examen_1/Program.cs
@@ -17,8 +17,8 @@ namespace Examen_1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-            Application.Run(new Form2());
-            //Application.Run(new Form3());
+            //Application.Run(new Form2());
+            Application.Run(new Form3());
 
         }
     }
